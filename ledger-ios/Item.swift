b20a838e7d78def4ccdd8096f3ac4a596ccaf22c
GrandNewStart <hbjw123@naver.com>
2025-06-22 12:55:28 +0900

    Initial Commit

---
+++ ledger-ios/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ledger-ios
+//
+//  Created by Jinwoo Hwangbo on 6/22/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
